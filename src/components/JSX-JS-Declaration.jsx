bd86e8a983d4ee6852da62d19405e0e86163748b
chhanda-chakrabarty <chhandachakrabarty7@gmail.com>
2025-02-24 00:31:45 +0600

    Dynamic Date-Time-Hour adding

--- src/components/JSX-JS-Declaration.jsx
+++ src/components/JSX-JS-Declaration.jsx
@@ -2,7 +2,13 @@ const name = 'chhanda';
 let todoDescription = "There is no one perfect but there is one above all!"
 let todoDate = "23-02-25";
 let dateTime = new Date();
-let currentDateTime = dateTime.getDate()
+let dateName = dateTime.getDate()
+let monthName = dateTime.getMonth()
+let currentYear = dateTime.getFullYear()
+let dayName = dateTime.getDay()
+let currentHour = dateTime.getHours()
+let currentMinutes = dateTime.getMinutes()
+let currentSeconds = dateTime.getSeconds()
 
 const First = () => {
   return (
@@ -11,7 +17,7 @@ const First = () => {
       <h1 className='bg-amber-500 text-5xl font-extrabold p-1 m-1 rounded '>{name}</h1>
       <p className='bg-red-400 text-2xl font-extrabold p-1 m-1 rounded'>{todoDescription}</p>
       <p className='text-red-600 text-9xl font-bold '>{todoDate}</p>
-      <p className='text-red-400 text-7xl p-1 m-1 font-bold'>{currentDateTime}</p>
+      <p className='text-red-400 text-7xl p-1 m-1 font-bold'>`{dateName} - {monthName} - {currentYear} - {dayName} - {currentHour} - {currentMinutes} - {currentSeconds}`</p>
     </div>
   )
 }
